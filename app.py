399658f96b612fade2a813d8e66b9dd74db226b7
AnaCatalinaSR <acsalazar_usistemas@intep.edu.co>
2025-11-26 09:19:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,9 @@ cookies = EncryptedCookieManager(
 if not cookies.ready():
     st.stop()
 
-# -------- LOGIN SCREEN --------
+
 # --- LOGO / PAGE CONFIG ---
+    
 logo_path = "logo-intep.png"
 if os.path.exists(logo_path):
     logo = Image.open(logo_path)
@@ -63,7 +64,7 @@ if os.path.exists(logo_path):
         st.image(logo, width=150)
 else:
     st.set_page_config(page_title="Gestión de Préstamos", layout="wide", page_icon="🏫")
-
+# -------- LOGIN SCREEN --------
 def login_screen():
     st.title("Inicio de Sesión")
     st.write("Acceso restringido al sistema de inventario.")
@@ -702,6 +703,7 @@ if st.sidebar.button("Cerrar sesión"):
     cookies.save()
     st.session_state["logged_in"] = False
     st.rerun()
+
 
 
 
